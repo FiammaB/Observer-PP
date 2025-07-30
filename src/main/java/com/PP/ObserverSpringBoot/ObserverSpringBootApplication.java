edf2c7a1f30e4790852afa376f9612764656d20d
Fiamma B <brizuelafiamma6@gmail.com>
2025-07-29 22:30:19 -0300

    Initial commit of Spring Boot project

---
+++ src/main/java/com/PP/ObserverSpringBoot/ObserverSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.PP.ObserverSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObserverSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObserverSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PP/ObserverSpringBoot/ObserverSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PP.ObserverSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObserverSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
